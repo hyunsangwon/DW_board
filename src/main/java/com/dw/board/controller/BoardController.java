01ecae6fa1d0e87e16a0892347ba33853a4e5a46
hyunsangwon <hyunsangwon93@gmail.com>
2022-06-03 14:30:32 +0900

    feat : jsp 수업 내용 업로드! (게시판 리스트)

--- src/main/java/com/dw/board/controller/BoardController.java
+++ src/main/java/com/dw/board/controller/BoardController.java
@@ -1,13 +1,37 @@
 package com.dw.board.controller;
 
+import java.util.List;
+import java.util.Map;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+import com.dw.board.service.BoardService;
+import com.github.pagehelper.PageInfo;
 
 @Controller
 public class BoardController {
+	
+	@Autowired
+	private BoardService boardService;
 	
 	@GetMapping("/home")
 	public String callHomepage() {
 		return "index";
 	}
+	
+	//http://localhost:8080/board?pageNum=1&pageSize=10
+	@GetMapping("/board")
+	public String callBoardPage(ModelMap model,
+			@RequestParam("pageNum") int pageNum, 
+			@RequestParam("pageSize") int pageSize) {
+		
+		List<Map<String, Object>> list = boardService.getAllBoardList(pageNum, pageSize);
+		model.addAttribute("pageHandler", new PageInfo<Map<String, Object>>(list));
+		return "board";
+	}
+		
 }
--- src/main/webapp/WEB-INF/views/index.jsp
+++ src/main/webapp/WEB-INF/views/index.jsp
@@ -1,13 +1,19 @@
-<%@ page language="java" contentType="text/html; charset=EUC-KR"
-    pageEncoding="EUC-KR"%>
+<%@ page language="java" contentType="text/html; charset=UTF-8"
+    pageEncoding="UTF-8"%>
+<%@ taglib prefix="c" uri="http://java.sun.com/jsp/jstl/core"%>
+<%@ taglib prefix="fn" uri="http://java.sun.com/jsp/jstl/functions"%>
 <!DOCTYPE html>
 <html>
 <head>
-<meta charset="EUC-KR">
-<title>Insert title here</title>
+<meta charset="UTF-8">
+<title>Dashboard</title>
 </head>
 <body>
 	<h1>Hello World!</h1>
 </body>
 <script type="text/javascript" src="/resources/static/js/index.js"></script>
+<script src="https://code.jquery.com/jquery-3.6.0.min.js" integrity="sha256-/xUj+3OJU5yExlq6GSYGSHk7tPXikynS7ogEvDej/m4=" crossorigin="anonymous"></script>
+<script type="text/javascript">
+
+</script>
 </html>
